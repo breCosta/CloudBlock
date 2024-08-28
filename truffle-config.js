05454d9ed457d69bc6d744fdc16f8798f28977a4
Brenda Louise Medeiros de Figueiredo Costa <67911648+breCosta@users.noreply.github.com>
2024-08-28 13:45:55 -0300

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,15 +1,15 @@
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",     // Endereço local
-      port: 8545,            // Porta padrão do Ganache
-      network_id: "*"        // Qualquer ID de rede
+      host: "127.0.0.1",   
+      port: 8545,            
+      network_id: "*"        
     }
   },
-  // Configure as versões de compilador necessárias
+  
   compilers: {
     solc: {
-      version: "0.8.0"      // Versão específica do compilador
+      version: "0.8.0"      
     }
   }
 };
